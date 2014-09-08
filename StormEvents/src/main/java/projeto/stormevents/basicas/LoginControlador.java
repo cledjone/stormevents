d4c7816258b413c25fb63512b592e53a091898ac
Bruno <Bruno@10.1.2.12>
2014-09-08 20:58:29 -0300

    Controlador do Login.

--- StormEvents/src/main/java/projeto/stormevents/basicas/LoginControlador.java
+++ StormEvents/src/main/java/projeto/stormevents/basicas/LoginControlador.java
@@ -1,5 +1,7 @@
 package projeto.stormevents.basicas;
 
+import java.util.List;
+
 import projeto.stormevents.dados.LoginDAO;
 import projeto.stormevents.dados.LoginIDAO;
 import projeto.stormevents.dados.UsuarioDAO;
@@ -12,4 +14,17 @@ public class LoginControlador {
  public LoginControlador() {
 	LoginDAO = new LoginDAO();
 			}	
+ public void inserirLogin (Login login){
+		LoginDAO.inserir(login);	
+	}
+	public List<Login> consultarTodos(){
+		return LoginDAO.consultarTodos();	
+	}
+	public void alterarLogin (Login login){
+		LoginDAO.alterar(login);	
+	}
+	public void removerUsuario (Login login){
+		LoginDAO.remover(login);
+	}
+
 }

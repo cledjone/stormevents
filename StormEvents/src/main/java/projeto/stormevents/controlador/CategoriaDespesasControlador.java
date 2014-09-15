ab504ef0073fd312ce74039f0894fab073e8c126
Eltinho <Eltinho@Eltinho-PC>
2014-09-15 15:12:05 -0300

    controlador de Categoria de Despesasas.

--- StormEvents/src/main/java/projeto/stormevents/controlador/CategoriaDespesasControlador.java
+++ StormEvents/src/main/java/projeto/stormevents/controlador/CategoriaDespesasControlador.java
@@ -1,5 +1,31 @@
 package projeto.stormevents.controlador;
 
-public class CategoriaDespesasControlador {
+import java.util.List;
+import projeto.stormevents.dados.CategoriaDespesasDAO;
+import projeto.stormevents.basicas.CategoriaDespesas;
 
+public class CategoriaDespesasControlador {
+	private CategoriaDespesasDAO categoriaDespesasDAO;
+
+	
+	
+	public void inserirUsuario(CategoriaDespesas cd) {
+		categoriaDespesasDAO.inserir(cd);
+	}
+
+	public List<CategoriaDespesas> consultarTodos() {
+		return categoriaDespesasDAO.consultarTodos();
+	}
+
+	public void alterarCategoriaDespesas(CategoriaDespesas cd) {
+		categoriaDespesasDAO.alterar(cd);
+	}
+
+	public void removerCategoriaDespesas(CategoriaDespesas cd) {
+		categoriaDespesasDAO.remover(cd);
+	}
+	
+	public CategoriaDespesas consultarporIdCategoriaDespesas(Integer id){
+		return categoriaDespesasDAO.consultarPorId(id);
+	}
 }
